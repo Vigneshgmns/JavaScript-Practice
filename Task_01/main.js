49fa1acf3f75f812fbe864a51f335b91208b8cb0
Vignesh G <148624660+Vigneshgmns@users.noreply.github.com>
2024-08-04 21:27:42 +0530

    complete the task

---
+++ Task_01/main.js
@@ -0,0 +1,9 @@
+function change() {
+    const ans = Number(document.getElementById('input').value)
+    const result = document.getElementById('result')
+    result.innerHTML = (ans * 9 / 5) + 32
+    console.log(result)
+}
+
+let str1='Lorem  vicky into the part'
+let str2='vio import of the current'
